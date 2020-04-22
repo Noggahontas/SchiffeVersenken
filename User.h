1678bba1304ae6bc06d6cea8451fca82bf05b119
Nora Hepp <nora.hepp@gmail.com>
2020-04-22 16:52:18 +0200

    remove bugged version

--- User.h
+++
@@ -1,7 +0,0 @@
-// User.h : header file
-//
-// Das Benutzer interface.
-/////////////////////////////////////////////////////////////////////////////
-
-void user_main();		// Funktion in der der Benutzer seinen eigenen
-					// Code programmieren kann.
